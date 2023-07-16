dd89802364f76251422b1a7ae0106a72efe79997
jnrsai24 <jnrsai24@gmail.com>
2023-07-16 12:14:41 +0000

    19 failed out of 36

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
-const App = () => <div>Hello World</div>
+import {Route, Switch} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import NotFound from './components/NotFound'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route component={NotFound} />s
+  </Switch>
+)
 
 export default App
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,53 @@
 // Write your code here
+import {Link} from 'react-router-dom'
+import Popup from 'reactjs-popup'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {IoMdClose} from 'react-icons/io'
+import {AiFillHome} from 'react-icons/ai'
+import {BsInfoCircleFill} from 'react-icons/bs'
+import './index.css'
+
+const Header = () => (
+  <div>
+    <Link to="/">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+        alt="website logo"
+      />
+    </Link>
+
+    <Popup
+      modal
+      trigger={
+        <button type="button">
+          <GiHamburgerMenu />
+        </button>
+      }
+    >
+      {close => (
+        <div className="popup">
+          <button type="button" onClick={close}>
+            <IoMdClose />
+          </button>
+          <div>
+            <button type="button" onClick={close}>
+              <Link to="/">
+                <AiFillHome />
+                Home
+              </Link>
+            </button>
+
+            <button type="button" onClick={close}>
+              <Link to="/about">
+                <BsInfoCircleFill />
+                About
+              </Link>
+            </button>
+          </div>
+        </div>
+      )}
+    </Popup>
+  </div>
+)
+
+export default Header
